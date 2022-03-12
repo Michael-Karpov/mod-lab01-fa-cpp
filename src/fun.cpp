8a55fc7f2bc4b41359b1e7e6fd63dc4bca67c706
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-12 20:43:39 +0300

    first function added

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,29 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int l = strlen(str) + 1;
+    int w = 0;
+    bool f = false;
+    
+    
+    for (int i = 0; i < l; i++) {
+    
+        if (isdigit(str[i])) {
+            f = true;
+        }
+
+        if (f == false && (str[i] == ' ' || str[i] == '\0')) {
+            w += 1;
+        }
+        
+        if (str[i] == ' ') {
+            f = false;
+            continue;
+        }
+
+
+    }
+    return w;
 }
 
 unsigned int faStr2(const char *str) {
