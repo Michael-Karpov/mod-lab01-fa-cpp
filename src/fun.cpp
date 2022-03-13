bb5a79a11d761a6500b5b3e40551ae83145daa3b
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-13 14:07:49 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,108 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <locale.h>
+#include <ctype.h>
+#include <stdio.h>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+        
+    int w = 0;
+    bool f = false;
+    int i = 0;
+    bool s = true;
+
+    while (str[i] == ' ' && str[i] != '\0') {
+        i++;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && f == false) {
+            f = true;
+            int k = i;
+            while (str[k] != ' ') {
+                if (isdigit(str[k])) {
+                    s = false;
+                    k++;
+                }
+                k++;
+            }
+            if (s == true) {
+                w++;
+            }
+        }
+        else if (str[i] == ' ') {
+            f = false;
+            s = true;
+        }
+        i++;
+    }
+
+    return w;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+      int i = 0;
+    int w = 0;
+    bool f = false;
+    bool s = true;
+
+    while (str[i] == ' ' && str[i] != '\0') {
+        i++;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && f == false) {
+            f = true;
+            if (isupper(str[i])) {
+                int k = i + 1;
+                while (str[k] != ' ') {
+                    if (!islower(str[k])) {
+                        s = false;
+                        k++;
+                    }
+                    k++;
+                }
+                if (s == true) {
+                    w++;
+                }
+            } 
+        }
+        else if (str[i] == ' ') {
+            f = false;
+            s = true;
+        }
+        i++;
+    }
+
+    return w;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+        int i = 0;
+    int w = 0;
+    int sym = 0;
+    bool f = false;
+    int x = 0;
+
+    while (str[i] == ' ' && str[i] != '\0') {
+        i++;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            sym++;
+        }
+        if (str[i] != ' ' && f == false) {
+            f = true;
+            w++;
+            int k = i;
+        }
+        else if (str[i] == ' ') {
+            f = false;
+        }
+        i++;
+    }
+
+    x = int(double(sym) / w + 0.5);
+    return x;
 }
