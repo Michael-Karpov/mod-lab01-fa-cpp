ce36ae4ec90ab006acdad8c40e077c90fa272494
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-13 12:19:05 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -30,7 +30,40 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int i = 0;
+    int w = 0;
+    bool f = false;
+    bool s = true;
+
+    while (str[i] == ' ' && str[i] != '\0') {
+        i++;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && f == false) {
+            f = true;
+            if (isupper(str[i])) {
+                int k = i + 1;
+                while (str[k] != ' ') {
+                    if (!islower(str[k])) {
+                        s = false;
+                        k++;
+                    }
+                    k++;
+                }
+                if (s == true) {
+                    w++;
+                }
+            } 
+        }
+        else if (str[i] == ' ') {
+            f = false;
+            s = true;
+        }
+        i++;
+    }
+
+    return w;
 }
 
 unsigned int faStr3(const char *str) {
